7c0eb1615979f3c38b749ed3131ec9bfff8efd76
Cristian Villalba <villalba.macri24@triana.salesianos.edu>
2025-11-07 12:41:54 +0100

    ARREGLAO 100%

--- proyecto-api-gonzalo-cristian/src/app/app.routes.ts
+++ proyecto-api-gonzalo-cristian/src/app/app.routes.ts
@@ -2,14 +2,13 @@ import { Routes } from '@angular/router';
 import { EventListComponent } from './components/event-list-component/event-list-component';
 import { EventDetail } from './pages/event-detail/event-detail';
 import { PageNotFound } from './pages/page-not-found/page-not-found';
-
-import { AttractionListComponent} from './components/attraction-list-component/attraction-list-component';
 import { AttractionItemComponent } from './components/attraction-item-component/attraction-item-component';
+import { AttractionListComponent} from './components/attraction-list-component/attraction-list-component';
 
 
 export const routes: Routes = [
   {
-    path: '',
+    path: 'events',
     component: EventListComponent,
     pathMatch: 'full',
   },
@@ -19,19 +18,17 @@ export const routes: Routes = [
     pathMatch: 'full',
   },
   {
-    path: '**',
-    component: PageNotFound,
-  },
-   { path: '', 
-    redirectTo: 'attractions', 
-    pathMatch: 'full' },
-  {
-    path:'attractions',
+    path: 'attractions',
     component: AttractionListComponent,
+    pathMatch: 'full',
   },
   {
     path: 'attractions/:id',
     component: AttractionItemComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: '**',
+    component: PageNotFound,
   }
 ];
-
