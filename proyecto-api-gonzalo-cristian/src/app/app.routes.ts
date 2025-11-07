e7b15a846a2744d26caabb966cbf71217760146e
Cristian Villalba <villalba.macri24@triana.salesianos.edu>
2025-11-07 11:55:15 +0100

    acabado

--- proyecto-api-gonzalo-cristian/src/app/app.routes.ts
+++ proyecto-api-gonzalo-cristian/src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { EventListComponent } from './components/event-list-component/event-list-component';
+import { EventDetail } from './pages/event-detail/event-detail';
+import { PageNotFound } from './pages/page-not-found/page-not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: EventListComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'events/:id',
+    component: EventDetail,
+    pathMatch: 'full',
+  },
+  {
+    path: '**',
+    component: PageNotFound,
+  },
+];
